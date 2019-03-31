67cf7d29638824c28342abf8dc287aee55955e4b
Romanuks <aleksejs.romanuks@accenture.com>
2019-03-31 02:28:55 +0200

    Spark streaming kafka consumer

--- build.sbt
+++ build.sbt
@@ -1,11 +1,24 @@
 name := "KafkaConsumer"
 
-version := "0.1.0"
+version := "0.1.1"
 
 scalaVersion := "2.11.8"
 
 
+val sparkVersion = "2.3.0"
 
+//libraryDependencies += "org.apache.kafka" %% "kafka" % sparkVersion
+//libraryDependencies += "org.apache.kafka" % "kafka-clients" % sparkVersion
 
-libraryDependencies += "org.apache.kafka" %% "kafka" % "2.2.0"
-libraryDependencies += "org.apache.kafka" % "kafka-clients" % "2.2.0"
+libraryDependencies += "org.apache.spark" %% "spark-streaming" % sparkVersion
+libraryDependencies += "org.apache.spark" %% "spark-streaming-kafka-0-10" % sparkVersion
+
+//libraryDependencies ++= Seq(
+//  "org.apache.spark" %% "spark-core" % sparkVersion,
+//  "org.apache.spark" %% "spark-streaming" % sparkVersion,
+//  "org.apache.spark" %% "spark-streaming-kafka-0-8" % "2.0.0"
+//)
+assemblyMergeStrategy in assembly := {
+  case PathList("META-INF", xs@_*) => MergeStrategy.discard
+  case x => MergeStrategy.last
+}
---
+++ src/main/scala/com/aleksejs/kafka/SparkStreamingConsumer.scala
@@ -0,0 +1,72 @@
+package com.aleksejs.kafka
+import org.apache.kafka.clients.consumer.{ConsumerConfig, ConsumerRecord}
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.spark.SparkConf
+import org.apache.spark.rdd.RDD
+import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
+import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
+import org.apache.spark.streaming.kafka010.KafkaUtils
+import org.apache.spark.streaming.{Seconds, StreamingContext, Time}
+import org.slf4j.LoggerFactory
+
+object SparkStreamingConsumer extends App {
+
+  val conf = new SparkConf().setAppName(SparkStreamingConsumer.getClass.getName)
+
+
+  val logger = LoggerFactory.getLogger(SparkStreamingConsumer.getClass.getName)
+
+  logger.warn("Initializing spark streaming context ... ")
+  val streamingContext = new StreamingContext(conf, Seconds(10))
+  streamingContext.sparkContext.setLogLevel("WARN")
+
+  logger.warn("Setting configurations for kafka consumer ... ")
+//  val kafkaParams = Map[String, Object](
+//    ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG -> "sandbox-hdp.hortonworks.com:6667",
+//    ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG -> classOf[StringDeserializer],
+//    ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG -> classOf[StringDeserializer],
+//    ConsumerConfig.GROUP_ID_CONFIG -> "com.uber.kafka.rightsOfAccess",
+//    ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG -> (false: java.lang.Boolean)
+//  )
+  val kafkaParams = Map[String, Object](
+    "bootstrap.servers" -> "sandbox-hdp.hortonworks.com:6667",
+    "key.deserializer" -> classOf[StringDeserializer],
+    "value.deserializer" -> classOf[StringDeserializer],
+    "group.id" -> "com.uber.kafka.rightsOfAccess",
+    "auto.offset.reset" -> "latest",
+    "enable.auto.commit" -> (false: java.lang.Boolean)
+  )
+
+  val topics = Array("test")
+
+
+  logger.warn("Creating kafka streaming ... ")
+  val kafkaStream = KafkaUtils.createDirectStream[String, String](
+    streamingContext,
+    PreferConsistent,
+    Subscribe[String, String](topics, kafkaParams)
+  )
+  kafkaStream.map(record=> record.value.toString).print
+  //kafkaMessages.map(record => record.value())
+  logger.warn("Staring input data transformations and loadings ... ")
+  kafkaStream.foreachRDD( (rdd: RDD[ConsumerRecord[String, String]], time: Time) =>
+    if(!rdd.isEmpty){
+
+      rdd.foreach(message =>
+        if(!message.value().isEmpty){
+          println("Message " + message.value.toString)
+          logger.warn(s"Message ${message.value.toString}")
+        }else{
+          logger.warn(s"Empty value")
+        }
+
+      )
+    }else{
+      logger.warn(s"Empty rdd")
+    }
+  )
+
+
+  streamingContext.start()
+  streamingContext.awaitTermination()
+}
